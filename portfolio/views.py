a03414a19dc6231c014946b7b48f1e53778a4a57
Mario Jasso <mario.jasso@e-vanhost.net>
2023-02-08 11:57:47 -0600

    first commit

---
+++ portfolio/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = "landing/home.html"
+
+class PortfolioView(TemplateView):
+    template_name = "landing/portfolio.html"
+
+# Create your views here.
